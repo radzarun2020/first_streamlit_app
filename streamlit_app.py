d6b89021b0ad5ec4ab13b344c49bc2ebfabe2b25
radzarun2020 <133000339+radzarun2020@users.noreply.github.com>
2023-05-09 06:03:45 -0400

    Update streamlit_app.py
    uploaded menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parents new Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+stremalit.text('Kale, Spinanch & Smoothie')
+stremalit.text('Hard-Boiled Free Range Egg')
